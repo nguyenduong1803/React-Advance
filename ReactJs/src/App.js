fe503aca39992f847514b6148fe0956c2d287263
duonnh200 <duongnh@sphinxjsc.com>
2022-12-15 10:36:16 +0700

    update

--- ReactJs/src/App.js
+++ ReactJs/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <h1>Cap nhat react</h1>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
