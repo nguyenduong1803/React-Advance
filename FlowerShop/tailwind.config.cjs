5b1e918531922b9894f721ffaeb2bb5c952ebfed
duonnh200 <duongnh@sphinxjsc.com>
2023-01-18 16:50:28 +0700

    feat: config base project esliint prettier import

---
+++ FlowerShop/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
+  theme: {
+    extend: {
+      colors: {
+        orange: '#ee4d2d'
+      }
+    }
+  },
+  plugins: [
+  
+  ]
+}
